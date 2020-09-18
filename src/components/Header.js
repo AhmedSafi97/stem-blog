f883536eefe4fa18e3b1a5d5203269238a178746
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-09-19 01:36:13 +0300

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react"
+
+import styles from "../styles/header.module.css"
+import Logo from "./Logo"
+import NavBar from "./NavBar"
+import Menu from "./Menu"
+import Search from "./Search"
+
+const Header = () => {
+  const [windowWidth, setWindowWidth] = useState()
+
+  useEffect(() => {
+    if (typeof window === "undefined") return
+
+    const handleResize = () => setWindowWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+  })
+
+  return (
+    <header className={styles.wrapper}>
+      <Logo />
+      {windowWidth > 1024 ? <NavBar /> : <Menu />}
+      <div className={styles.search_component}>
+        <Search />
+      </div>
+    </header>
+  )
+}
+
+export default Header
