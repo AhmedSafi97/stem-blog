fe9578d75624e67aad739eccdb9644287165c438
Ahmed Safi <ahmadhsafi1997@gmail.com>
2020-09-18 13:09:11 +0300

    created file structure and set up Gatsby project (#15)

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `STEM blog`,
+        short_name: `STEM`,
+        start_url: `/`,
+        background_color: `#E5E5E5`,
+        theme_color: `#E5E5E5`,
+        display: `standalone`,
+        icon: `src/assets/icon.svg`,
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
+}
