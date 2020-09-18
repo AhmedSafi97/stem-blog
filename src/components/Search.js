84b0e7500aab2bb68eedbe825ca67d5e891c4ec5
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-09-19 01:36:52 +0300

    fixed search component styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,14 @@ import React from "react"
 
 import styles from "../styles/search.module.css"
 
-const Search = ({onChange}) => (
+const Search = ({ onChange }) => (
   <div>
     <span className={styles.wrapper}>
       <input
         type="search"
         placeholder="Search"
         aria-label="Search articles"
-        autocomplete="off"
+        autoComplete="off"
         className={styles.input}
         onChange={onChange}
       />
