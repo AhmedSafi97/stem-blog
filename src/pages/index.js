8ed3959f6de6a57d8328e0cae5db54072c283e64
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-09-19 19:08:44 +0300

    wrote query in the home page to pull md files and pass articles frontmatter down to Latest article component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const Home = () => <h1>STEM</h1>
+import styles from "../styles/home.module.css"
+import { Layout, LatestArticle } from "../components"
+
+const Home = ({ data }) => {
+  console.log(data)
+
+  return <h1>STEM</h1>
+}
 
 export default Home
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "dd mm yyyy")
+          title
+          category
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
